8cc59adfb5222e4a397c43f0253fc8547acbc988
87tawanzinho <87tawanzinho@gmail.com>
2023-12-12 00:22:16 -0300

    first commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="flex justify-between p-8 text-gray-600">
+      <p className="cursor-pointer hover:text-gray-700">Inicio</p>
+      <p className="cursor-pointer hover:text-gray-700">Sobre Mim</p>
+      <p className="cursor-pointer hover:text-gray-700">Projetos</p>
+      <p className="cursor-pointer hover:text-gray-700">Contato</p>
+    </header>
+  );
+}
